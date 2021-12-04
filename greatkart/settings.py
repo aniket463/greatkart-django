0f20d1ca61857c1557c8fe0829b1de9cff81170b
unknown <aniketghosh810@gmail.com>
2021-12-04 21:13:23 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL  ='/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP CONFIGURATIONS
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aniketofficial48@gmail.com'
+EMAIL_HOST_PASSWORD = 'Buban810@'
+EMAIL_USE_TLS = 'True'
